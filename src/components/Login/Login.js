9147efead3bff7dc870d1e9cc9565cf5ac0b58ec
sergiososa <Sergio Sosa>
2020-10-16 11:37:15 -0500

    Se ajusto el gitignore para no incluir package-lock.json, se agrego el logo de cuidamed en jpg

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import { Container, Header, Label, Input, Button, Image } from 'semantic-ui-react'
 // import Imagen from "../Image"
-import Logo from "../../images/logo.jpg"
+import Logo from "../../images/CuidamedLogo.png"
 import "./Login.scss"
 
 export default function Login() {
     return (
         <div>
             <Header className="header">
-                <div class="register">
-                    <p>¿No tienes cuenta? <a href="#"><span>Crea cuenta</span></a></p>
+                <div className="register">
+                    <p>¿No tienes cuenta? <a href="#asda"><span>Crea cuenta</span></a></p>
                 </div>
-                <Image src={Logo} className="logo"/>
+                <Image src={Logo} className="logo" />
                 {/* <Imagen fileName="logo.jpg" alt="logo-cuidamed" className="logo"/> */}
             </Header>
-            <Container className="container">        
-                
+            <Container className="container">
+
                 <Label>Entrar a la cuenta</Label>
                 <Input focus placeholder='Correo electrónico' />
-                <Input placeholder='Contraseña'/>
+                <Input placeholder='Contraseña' type="password" />
 
                 <Button>Iniciar sesión</Button>
                 <Button className="emergency">Emergencia</Button>
