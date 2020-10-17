8da1a997d6f4b08b8150c091eb309030fd0457ba
LuisAlpachino <alamilla-96@hotmail.com>
2020-10-16 22:16:32 -0500

    NavBar Secundario e input text component

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,16 +26,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.jpg`, // This path is relative to the root of the site.
       },
-    },
-    {
-      resolve: `gatsby-plugin-react-svg`,
-      options: {
-        rule: {
-          include: /images\/icons/, 
-        }
-      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,6 @@ import BasicLayout from "../layouts/BasicLayout";
 export default function Home() {
   return (
     <BasicLayout>
-      <h2>This is the index</h2>
-      <p>soy una p</p>
     </BasicLayout>
   )
 }
