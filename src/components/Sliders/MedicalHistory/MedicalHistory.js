35a5773999134bdd34a20f43fe5170b4636c4d7b
LuisAlpachino <alamilla-96@hotmail.com>
2020-11-06 18:48:45 -0600

    se añadio la maquetacion de la pantalla enfermedades comunes

--- src/components/Sliders/MedicalHistory/MedicalHistory.js
+++ src/components/Sliders/MedicalHistory/MedicalHistory.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react"
 import { navigate } from 'gatsby'
 
 import Moment from 'react-moment';
-import { Grid, Container, Icon, Button } from "semantic-ui-react"
+import { Grid, Container, Icon, Button, GridColumn } from "semantic-ui-react"
 
 
 // import Swiper core and required components
@@ -16,16 +16,35 @@ import "swiper/components/navigation/navigation.scss"
 import "swiper/components/pagination/pagination.scss"
 import "swiper/components/scrollbar/scrollbar.scss"
 
+import { commonDiseases } from "./data"
+
 // import "./Slider.scss"
 // install Swiper components
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
 
 export default function MedicalHistory() {
 
+    useEffect(() => {
+        console.log(listCommonDiseases);
+    }, [listCommonDiseases])
+
+    const [listCommonDiseases, setListCommonDiseases] = useState([])
+    const handleCheckedInput = e => {
+        console.log(e.currentTarget.checked);
+        console.log(listCommonDiseases)
+        const value = e.target.value
+
+        if (e.currentTarget.checked) {
+            setListCommonDiseases(listCommonDiseases => [...listCommonDiseases, value])
+        }
+
+    }
+
+
     return (
         <Grid centered className="slider">
             <h1 className="title">Historial Médico</h1>
-            <Grid.Row columns={16}>
+            <Grid.Row >
                 <Swiper
                     spaceBetween={50}
                     slidesPerView={1}
@@ -40,13 +59,25 @@ export default function MedicalHistory() {
                     simulateTouch={false}
                 >
                     <SwiperSlide>
-                        <h2>¿Tienes algunas de estas enfermedades?</h2>
-                        <div className="container-enfermedades">
-                            <div className="">
-                                <input type="checkbox" id="enfermedad1"></input>
-                                <label htmlFor="enfermedad1" className="ui button" placeholder="hipertension">Hipertensión</label>
-                            </div>
-                        </div>
+                        <Container >
+                            <Grid>
+                                <Grid.Column computer={16} mobile={16} tablet={16}>
+                                    <Grid.Row>
+                                        <h2 className="title-diseas">¿Tienes algunas de estas enfermedades?</h2>
+                                    </Grid.Row>
+                                    <Grid.Row>
+                                        <div className="container-enfermedades">
+                                            {commonDiseases.map((disease, index) => (
+                                                <div key={index} className="container-enfermedades__btn">
+                                                    <input type="checkbox" id={disease.id} value={disease.id} onChange={handleCheckedInput}></input>
+                                                    <label htmlFor={disease.id} className="ui button">{disease.name}</label>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </Grid.Row>
+                                </Grid.Column>
+                            </Grid>
+                        </Container>
                     </SwiperSlide>
                     <SwiperSlide>
 
